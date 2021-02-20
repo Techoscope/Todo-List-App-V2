d09557f2f3a5b66aeade7606e560aa3f9d199864
Cemil Okay <cemilokay@gmail.com>
2021-02-20 21:05:48 +0000

    Add todo item into the list

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 // When a user enter the name of the item and click add button, the item will be added to the list.
 
+function addItem (){
+  // alert(document.getElementById('todo_input').value);
+  const item = document.createElement('li');
+  item.innerHTML = document.getElementById('todo_input').value;
+  document.getElementById('todo_list').appendChild(item);
+ }
+
 // When a user click on the item in the list, the item will be removed/deleted.
+
+//node
