d2559c6378e2a445eb671522002c8217ef8b9075
Cemil Okay <cemilokay@gmail.com>
2021-02-20 22:14:27 +0000

    Refactoring

--- script.js
+++ script.js
@@ -1,23 +1,16 @@
-// When a user enter the name of the item and click add button, the item will be added to the list.
+// Task 1 - When a user enter the name of the item and click add button, the item will be added to the list.
+// Task 2 - When a user click on the item in the list, the item will be removed/deleted.
 
 function addItem (){
-  // alert(document.getElementById('todo_input').value);
   if (document.getElementById('todo_input').value) {
     const item = document.createElement('li');
     item.innerHTML = document.getElementById('todo_input').value;
-    item.onclick = () => removeItem(item);
-    // item.onclick = function() {
-    //   removeItem(item)
-    // };
+    item.onclick = function() {
+      document.getElementById('todo_list').removeChild(item);
+    };
     document.getElementById('todo_list').appendChild(item);
     document.getElementById("todo_input").value = "";
   } else {
     alert('Enter an item')
   }
  }
-
-// When a user click on the item in the list, the item will be removed/deleted.
-
-function removeItem(item) {
-  document.getElementById('todo_list').removeChild(item);
-}
