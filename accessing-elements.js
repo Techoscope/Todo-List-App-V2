02a3eb7e438b5a90fe9660e8a6bbb742cd69a086
Cemil Okay <cemilokay@gmail.com>
2021-02-20 20:14:17 +0000

    Initialized the project

---
+++ accessing-elements.js
@@ -0,0 +1,14 @@
+// const pageTitle = document.title;
+// const body = document.body;
+// const title = document.getElementById('title');
+// const description = document.getElementsByClassName('description');
+// const button = document.getElementsByTagName('button');
+// const input = document.getElementsByName('todo_item');
+// const title = document.querySelector('#title');
+// const description = document.querySelectorAll('.description');
+// const button = document.querySelectorAll('button');
+
+// console.log(title.innerHTML);
+// console.log(description[0].innerHTML);
+// console.log(button[1].innerHTML);
+// console.log(input[0].value);
---
+++ script.js
@@ -0,0 +1,3 @@
+// When a user enter the name of the item and click add button, the item will be added to the list.
+
+// When a user click on the item in the list, the item will be removed/deleted.
