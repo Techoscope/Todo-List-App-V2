0870aab91fb2a756b86f9de9401d457129cd0af4
Cemil Okay <cemilokay@gmail.com>
2021-02-20 22:57:09 +0000

    Implement local storage

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 // Task 1 - When a user enter the name of the item and click add button, the item will be added to the list.
 // Task 2 - When a user click on the item in the list, the item will be removed/deleted.
 
+const itemList = localStorage.getItem('todoItems').split(',');
+
+itemList.forEach(element => {
+  const item = document.createElement('li');
+  item.innerHTML = element;
+  document.getElementById('todo_list').appendChild(item);
+});
+
 function addItem (){
   if (document.getElementById('todo_input').value) {
     const item = document.createElement('li');
@@ -9,6 +17,10 @@ function addItem (){
       document.getElementById('todo_list').removeChild(item);
     };
     document.getElementById('todo_list').appendChild(item);
+    
+    itemList.push(document.getElementById('todo_input').value);
+    localStorage.setItem('todoItems', itemList);
+
     document.getElementById("todo_input").value = "";
   } else {
     alert('Enter an item')
