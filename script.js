1e6655c99b26e1b8f6c20d24a4edb8fbc1fc5c4a
Cemil Okay <cemilokay@gmail.com>
2021-02-23 02:46:12 +0000

    Append the input value to UL element

--- script.js
+++ script.js
@@ -1,13 +1,9 @@
-const list = ['Tidy your room','Have a breakfast','Hangout with folks'];
-
 // const ulList = document.querySelector('ul');
 const ulList = document.getElementById('todo_list');
 
-list.forEach(item => {
-  // ulList.innerHTML += `<li>${item}</li>`;
-  const listItem = document.createElement('li');
-  listItem.innerHTML = item;
-  ulList.appendChild(listItem);
-})
+const inputValue = document.getElementById('todo_input').value;
 
-console.log(ulList);
+const listItem = document.createElement('li');
+listItem.innerHTML = inputValue;
+ulList.appendChild(listItem)
+
