e320219442ac769b9c7d1aecdd2b56341450dbed
Cemil Okay <cemilokay@gmail.com>
2021-02-23 02:49:48 +0000

    Add onclick event to addItem()

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 // const ulList = document.querySelector('ul');
 const ulList = document.getElementById('todo_list');
 
-const inputValue = document.getElementById('todo_input').value;
+function addItem() {
+  const inputValue = document.getElementById('todo_input').value;
 
-const listItem = document.createElement('li');
-listItem.innerHTML = inputValue;
-ulList.appendChild(listItem)
-
+  const listItem = document.createElement('li');
+  listItem.innerHTML = inputValue;
+  ulList.appendChild(listItem)
+}
