643f9934db2d6d1f475d3ba7186a464001879b24
Cemil Okay <cemilokay@gmail.com>
2021-02-27 19:31:22 +0000

    Separate local-storage.js file

---
+++ local-storage.js
@@ -0,0 +1,34 @@
+// localStorage.setItem('myCat', 'Minosh'); // Setting value
+// const myCat =  localStorage.getItem('myCat'); //Getting value
+// localStorage.removeItem('todoItems'); // Remove single item at a time
+// localStorage.clear(); // Removes every items
+// alert(myCat)
+
+const todoItems = [];
+
+function addItem() {
+  const inputBox = document.getElementById('todo_input');
+  todoItems.push(inputBox.value);
+  localStorage.setItem('todoItems', todoItems);
+  getItem();
+}
+
+function getItem() {
+  const itemsToList = localStorage.getItem('todoItems').split(',');
+  document.getElementById('todo_list').innerHTML = '';
+  itemsToList.forEach((item) => {
+    document.getElementById('todo_list').innerHTML += `<li>${item}</li>`;
+  })
+}
+
+getItem();
+
+
+// function addItem() {
+//   const inputBox = document.getElementById('todo_input');
+//   localStorage.setItem('todoItem', inputBox.value);
+  // console.log(localStorage.getItem('todoItem'))
+// }
+
+// const myCat = 'Minosh'; // Setting value
+// alert(myCat); // Getiing value
