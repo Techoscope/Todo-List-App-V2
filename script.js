4c289bb8c714d772f3443e50d5c6a5e0c031af41
Cemil Okay <cemilokay@gmail.com>
2021-03-19 01:33:39 +0000

    Remove item with XHR request

--- script.js
+++ script.js
@@ -28,9 +28,27 @@ function listItem(todoItems) {
 
   todoItems.forEach((item) => {
     // console.log(item.title)
-    ulList.innerHTML += `<li> ${item.title} </li>`
+    // ulList.innerHTML += `<li onclick="removeItem(this)"> ${item.title} </li>`
+    const listItem = document.createElement('li');
+    listItem.innerHTML = item.title;
+    listItem.id = item.id;
+    // listItem.onclick = removeItem(listItem);
+    listItem.addEventListener('click', removeItem);
+    ulList.appendChild(listItem);
   });
+}
 
+function removeItem(e) {
+  const xhr = new XMLHttpRequest();
+  const url = 'http://127.0.0.1:8080/api/todoitems/' + e.target.id;
+  xhr.responseType = 'json';
+  xhr.onreadystatechange = () => {
+    if (xhr.readyState === XMLHttpRequest.DONE){
+      e.target.remove()
+    }
+  }
+  xhr.open('DELETE', url);
+  xhr.send();
 }
 
 getItems();
